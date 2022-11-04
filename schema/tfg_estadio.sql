25e05a1f4ba22562c6333eb9ccd9403526cbffa0
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2022-11-04 09:35:08 +0100

    SQL schema
    The schema that we will be using for our test.

---
+++ schema/tfg_estadio.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tfg
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `estadio`
+--
+
+DROP TABLE IF EXISTS `estadio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estadio` (
+  `idEstadadio` int NOT NULL,
+  `NombreEstadio` varchar(45) NOT NULL,
+  `Capacidad` int NOT NULL,
+  `Ubicacion` varchar(45) NOT NULL,
+  `Secciones` int DEFAULT NULL,
+  `Grupos` int DEFAULT NULL,
+  `Filas` int DEFAULT NULL,
+  `Asientos` int DEFAULT NULL,
+  `PorcentajeLLeno` int GENERATED ALWAYS AS (0) VIRTUAL,
+  PRIMARY KEY (`idEstadadio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estadio`
+--
+
+LOCK TABLES `estadio` WRITE;
+/*!40000 ALTER TABLE `estadio` DISABLE KEYS */;
+INSERT INTO `estadio` (`idEstadadio`, `NombreEstadio`, `Capacidad`, `Ubicacion`, `Secciones`, `Grupos`, `Filas`, `Asientos`) VALUES (0,'Civitas Metropolitano',68456,'madrid',5,39,20,18),(1,'Santiago Bernabeu',81044,'madrid',7,46,20,18);
+/*!40000 ALTER TABLE `estadio` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-04  9:32:52
