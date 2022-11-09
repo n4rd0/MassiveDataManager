2936311971aa7d3de280b9db1b0c73eaee7f037f
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2022-11-09 15:25:17 +0100

    Update Conector.py

--- Conector.py
+++ Conector.py
@@ -12,6 +12,6 @@ def establecerConexion(host, usuario, contrasenia, bbdd):
     #conexion a la bbdd 
     db = pymysql.connect(host = 'localhost', 
                          user = 'root', 
-                         passwd = '7821', 
-                         database = 'tfg')
+                         passwd = '', 
+                         database = '')
     return db
